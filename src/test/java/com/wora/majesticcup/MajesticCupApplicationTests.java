b9fa3ae70ca732b461a618cfe5b21cded7714ea8
hamzalamin <hamzalamin80@gmail.com>
2024-12-06 08:38:44 +0100

    Set up environment of project MajisticCup

---
+++ src/test/java/com/wora/majesticcup/MajesticCupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wora.majesticcup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajesticCupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
